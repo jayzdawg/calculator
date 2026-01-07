e0d29ea0af0faff131520ad5a1bb04440c0c0766
Jaden Zon <jaden8blue@yahoo.ca>
2026-01-07 11:49:57 +0530

    Chained operators functioning, added AC button function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber;
+let secondNumber;
 let operator;
 let result;
-let processStage = "initialStage";
+let processStage = "stageOne";
+let clearScreen = false;
+
+//stageOne is the initial stage of the calculator where the operator is not selected and the first number data is being collected.
+//stageTwo means the operator has been selected and the second number data is being collected.
 
 const inputField = document.getElementById('screen');
     
@@ -13,35 +17,38 @@ document.querySelectorAll('.numbers button').forEach(button => {
         // Get the data-value from the clicked button
         const buttonValue = this.getAttribute('data-value');
 
-        
-        
-
-        if (processStage == "initialStage"){
-        inputField.value += buttonValue;    // Append the value to the input field
-        firstNumber = parseInt(inputField.value);
-        } else if (processStage == "operateStage"){
+        if (clearScreen == true){
         inputField.value = "";
-        inputField.value += buttonValue;
-        processStage = "secondStage";
-        } else if (processStage == "secondStage"){
-        inputField.value += buttonValue;
-        secondNumber = parseInt(inputField.value);
-        }
-        
-        
+        clearScreen = false;    
+        } 
+
+        inputField.value += buttonValue;    // Append the value to the input field
         
     });
 });
 
+// If the operator is has not been selected, it will save as 
+
 //Operator buttons
 document.querySelectorAll('.operator-btn').forEach(button => {
     button.addEventListener('click', function() {
+
         const operatorValue = this.getAttribute('data-value');
+     
+        if (processStage == "stageOne"){
+        firstNumber = parseFloat(inputField.value);
         operator = operatorValue;
-        inputField.value += operator;
-        processStage = "operateStage";
-        
-    
+        clearScreen = true;
+        processStage = "stageTwo";
+        } else if (processStage == "stageTwo"){
+        secondNumber = parseFloat(inputField.value);
+        clearScreen = true;
+        operate(operator);
+        operator = operatorValue;
+        inputField.value = result;
+        firstNumber = result;
+        }
+
     });
 
 })
@@ -51,18 +58,36 @@ document.querySelectorAll('.operator-btn').forEach(button => {
 const equals = document.getElementById('=');
   
     equals.addEventListener('click', function() {
+     if (operator == null || secondNumber == null){
+        error();
+    }
+    secondNumber = parseFloat(inputField.value);
     operate(operator);
     inputField.value = result;
     firstNumber = result;
-
+    processStage = "stageOne";
     });
 
-    
+
+const clearButton = document.getElementById('butt ac');    
+    clearButton.addEventListener('click', function() {
+    reset();
+      });
+
 
 //error message
 
 function error(){
-    inputField.value = "error";
+    inputField.value = "excuse me?";
+}
+
+function reset(){
+    inputField.value = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = null;
+    processStage = "stageOne";
+    result = null;
 }
 
 //ADD FUNCTION add number 1 and number 2. Triggered if operator == + when they hit equal. Same code for other operators.
@@ -70,15 +95,12 @@ function error(){
 function addition(firstNumber, secondNumber){
     return firstNumber + secondNumber;
 }
-
 function subtraction(firstNumber, secondNumber){
     return firstNumber - secondNumber;
 }
-
 function multiplication(firstNumber, secondNumber){
     return firstNumber * secondNumber;
 }
-
 function division(firstNumber, secondNumber){
     return firstNumber / secondNumber;
 }
@@ -96,6 +118,7 @@ function operate(operator){
         result = multiplication(firstNumber, secondNumber);
     } else if (operator == "/"){
         result = division(firstNumber, secondNumber);
+       
     } 
 
 }
